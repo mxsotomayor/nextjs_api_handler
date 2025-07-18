e1d6582612a3722847382c539b1a42fac43f56c6
maxwell <cubanmaxramos@gmail.com>
2025-07-18 12:22:40 -0100

    adding build

--- index.ts
+++ index.ts
@@ -1,4 +1,1 @@
-export * from './src'
-export * from './src/types'
-export * from './src/helpers'
-export * from './src/http/responses'
+export * from './dist'
--- settings.ts
+++
@@ -1,5 +0,0 @@
-const nextApiHandler = {
-  dispatcherBasePath: "./src/app/(web_api)/api",
-  apiHandlerDir: "./src/next_api_handler",
-};
-export default nextApiHandler;
